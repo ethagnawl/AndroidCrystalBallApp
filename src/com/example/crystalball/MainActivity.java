234f195df5e0825d68f830fd32873d1173738be2
Pete Doherty <ethagnawl@gmail.com>
2013-12-23 22:19:31 -0500

    fixing whitespace in MainActivity.java

--- src/com/example/crystalball/MainActivity.java
+++ src/com/example/crystalball/MainActivity.java
@@ -15,62 +15,62 @@ import com.example.crystalball.R.id;
 import com.example.crystalball.ShakeDetector.OnShakeListener;
 
 public class MainActivity extends Activity {
-	public static final String TAG = MainActivity.class.getSimpleName(); 
-	private CrystalBall mCrystalBall = new CrystalBall();
-	private TextView mAnswerLabel;
-	private ImageView mCrystalBallImage;
-	private SensorManager mSensorManager;
-	private Sensor mAccelerometer;
-	private ShakeDetector mShakeDetector;
-	
-	@Override
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private CrystalBall mCrystalBall = new CrystalBall();
+    private TextView mAnswerLabel;
+    private ImageView mCrystalBallImage;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-                
+
         mAnswerLabel = (TextView) findViewById(id.textView1);
-    	mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
-    	
-    	mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-    	mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    	mShakeDetector = new ShakeDetector(new OnShakeListener() {
-			
-			@Override
-			public void onShake() {
-				handleNewAnswer();
-			}
-		});    
-    	
-    	Log.d(TAG, "We're logging from #onCreate.");
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-    	mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
-	}
-	
-	@Override
-	public void onPause() {
-		super.onPause();		
-    	mSensorManager.unregisterListener(mShakeDetector);
-	}
-    
-    private void animateCrystalBall() {
-    	mCrystalBallImage.setImageResource(R.drawable.ball_animation);
-    	AnimationDrawable ballAnimation = (AnimationDrawable) mCrystalBallImage.getDrawable();
-    	if (ballAnimation.isRunning()) {
-    		ballAnimation.stop();
-    	}
-    	ballAnimation.start();
+        mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector(new OnShakeListener() {
+            @Override
+            public void onShake() {
+                handleNewAnswer();
+            }
+        });
+
+        Log.d(TAG, "We're logging from #onCreate.");
     }
-    
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(mShakeDetector);
+    }
+
+    private void animateCrystalBall() {
+        mCrystalBallImage.setImageResource(R.drawable.ball_animation);
+        AnimationDrawable ballAnimation = (AnimationDrawable) mCrystalBallImage.getDrawable();
+
+        if (ballAnimation.isRunning()) {
+            ballAnimation.stop();
+        }
+
+        ballAnimation.start();
+    }
+
     private void animateAnswer() {
-    	AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1); 
-    	fadeInAnimation.setDuration(1500);
-    	fadeInAnimation.setFillAfter(true);
-    	
-    	mAnswerLabel.setAnimation(fadeInAnimation);
+        AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+
+        fadeInAnimation.setDuration(1500);
+        fadeInAnimation.setFillAfter(true);
+        mAnswerLabel.setAnimation(fadeInAnimation);
     }
 
     @Override
@@ -80,11 +80,11 @@ public class MainActivity extends Activity {
         return true;
     }
 
-	private void handleNewAnswer() {
-		String answer = mCrystalBall.getAnAnswer();
-		mAnswerLabel.setText(answer);
-		animateCrystalBall();
-		animateAnswer();
-	}
-    
+    private void handleNewAnswer() {
+        String answer = mCrystalBall.getAnAnswer();
+
+        mAnswerLabel.setText(answer);
+        animateCrystalBall();
+        animateAnswer();
+    }
 }
